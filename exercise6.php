a198f2f67f1ca11ca83031cc6e833bdf6350d8bf
adhermit <maharjanraman456@gmail.com>
2025-01-15 17:00:18 +0100

    update

--- exercise6.php
+++ exercise6.php
@@ -9,3 +9,49 @@
  * Implement a method to display all items.
  * Implement a method to calculate the total cost.
  */
+class ShoppingCart{
+    protected array $items;
+    protected int $total;
+
+    public function __construct(array $items, int $total)
+    {
+        $this->setItems($items);
+        $this->setTotal($total);        
+    }
+
+    /**
+     * Get the value of items
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * Set the value of items
+     */
+    public function setItems(array $items): self
+    {
+        $this->items = $items;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of total
+     */
+    public function getTotal(): int
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set the value of total
+     */
+    public function setTotal(int $total): self
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+}
